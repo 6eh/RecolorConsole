ae44841d40ab85f3334a7ac18e377719b297e1a6
6eh Git CSharp Test <mcls@list.ru>
2022-12-21 18:16:49 +0300

    Module 4, Unit 4.1, Task 4.1.17

--- RecolorConsole/Program.cs
+++ RecolorConsole/Program.cs
@@ -8,7 +8,7 @@ namespace RecolorConsole
         {
             Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
 
-            var color = Console.ReadLine();
+            /*var color = Console.ReadLine();
 
             if (color == "red")
             {
@@ -31,6 +31,35 @@ namespace RecolorConsole
                 Console.ForegroundColor = ConsoleColor.Black;
 
                 Console.WriteLine("Your color is cyan!");
+            }*/
+
+            var color = Console.ReadLine();
+
+            switch(color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Your color is red!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Your color is green!");
+                    break;
+
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor= ConsoleColor.Red;
+                    Console.WriteLine("Your color is yellow!");
+                    break;
             }
         }
     }
